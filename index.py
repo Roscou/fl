4d9fdd3d8f8cb489627dee8fa1452aabc07d0b54
Roscou <nvv524@mail.ru>
2019-05-14 17:20:41 +0300

    first commit

--- index.py
+++ index.py
@@ -3,4 +3,5 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+   
+   return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
